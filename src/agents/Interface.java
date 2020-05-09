ab8128746c9ed2cb44179b06131d4128aba0a1d3
BM-a81824 <BM-a81824@users.noreply.github.com>
2020-05-09 14:18:21 +0100

    barbs

--- src/agents/Interface.java
+++ src/agents/Interface.java
@@ -18,6 +18,7 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
+import messages.message_estacao_inform;
 import messages.message_utilizador_request;
 
 import java.util.HashMap;
@@ -27,7 +28,8 @@ public class Interface extends Agent{
 	
 	private HashMap<AID, ArrayList> info_estacao; //identificação, coordenadas
 	private HashMap<AID, ArrayList> info_utilizadores; //identificação, coordenadas
-	private ArrayList<Integer> coordenadas; //coordenadas e area da APE
+	private ArrayList<Integer> coordenadas_u; //coordenadas
+	private ArrayList<Integer> coordenadas_e; //coordenadas e area da APE
 	
 	protected void setup() {
 		super.setup();
@@ -38,7 +40,6 @@ public class Interface extends Agent{
 	protected void takeDown() {
 		super.takeDown();
 	}
-	
 	
 	
 	private class Utilizador_Request_Receiver extends CyclicBehaviour {
@@ -58,21 +59,21 @@ public class Interface extends Agent{
 					y_u_destino = content.getY_destination();
 					id_utilizador = content.getAgent();
 					
-					coordenadas = new ArrayList<Integer>();
-					coordenadas.add(x_u);
-					coordenadas.add(y_u);
-					coordenadas.add(x_u_destino);
-					coordenadas.add(x_u_destino);
+					coordenadas_u = new ArrayList<Integer>();
+					coordenadas_u.add(x_u);
+					coordenadas_u.add(y_u);
+					coordenadas_u.add(x_u_destino);
+					coordenadas_u.add(x_u_destino);
 					
 					info_utilizadores = new HashMap<AID, ArrayList>();
 					if (info_utilizadores.containsKey(id_utilizador)) {
-						info_utilizadores.replace(id_utilizador, coordenadas);
+						info_utilizadores.replace(id_utilizador, coordenadas_u);
 					} else {
-						info_utilizadores.put(id_utilizador, coordenadas);
+						info_utilizadores.put(id_utilizador, coordenadas_u);
 					}
 					
 					System.out.println("Interface: info do utilizador");
-					System.out.println("________________"+id_utilizador.getLocalName()+"____________");
+					System.out.println("____________"+id_utilizador.getLocalName()+"____________");
 					System.out.println("origem: "+ x_u +", "+ y_u);
 					System.out.println("destino: "+ x_u_destino +", "+ y_u_destino);
 					
@@ -83,10 +84,58 @@ public class Interface extends Agent{
 				
 			}
 		}
-
+	}
+	
+	private class Estacao_Info_Receiver extends CyclicBehaviour {
+		private int x_e, y_e;
+		private AID id_estacao;
+		private int nr_estacoes;
+		private int nr_docas;
+		private int ape;
+		private AID id_bicicleta;
+		private int nr_bicicletas_disp;
 		
-		
 
+		public void action() {
+			ACLMessage msg = receive();
+			if (msg != null && msg.getPerformative() == ACLMessage.INFORM) {
+				System.out.println("Interface: estação informou");
+				
+				try {
+					message_estacao_inform content = (message_estacao_inform) msg.getContentObject();
+					x_e = content.getX();
+					y_e = content.getY();
+					id_estacao = content.getAgent();
+					nr_bicicletas_disp = content.getNr_bike_avail();
+					nr_estacoes = content.getNr_stations();
+					nr_docas = content.getNr_docks();
+					ape = content.getApe();
+					id_bicicleta = content.getId_bike();
+					nr_bicicletas_disp = content.getNr_bike_avail();
+					
+					coordenadas_e = new ArrayList<Integer>();
+					coordenadas_e.add(x_e);
+					coordenadas_e.add(y_e);
+					coordenadas_e.add(ape);
+					
+					info_estacao = new HashMap<AID, ArrayList>();
+					if (info_estacao.containsKey(id_estacao)) {
+						info_estacao.replace(id_estacao, coordenadas_e);
+					} else {
+						info_utilizadores.put(id_estacao, coordenadas_e);
+					}
+					
+					System.out.println("Interface: info da estacao");
+					System.out.println("____________"+id_estacao.getLocalName()+"____________");
+					System.out.println("coordenadas: "+ x_e +", "+ y_e);
+					System.out.println("APE: "+ ape);
+					
+				} catch (UnreadableException e1) {
+					e1.printStackTrace();
+				}
+					
+			}
+		}
 	}
 
 }
--- src/messages/message_estacao_inform.java
+++ src/messages/message_estacao_inform.java
@@ -7,22 +7,22 @@ public class message_estacao_inform implements java.io.Serializable {
 	public AID agent;
 	public int x;
 	public int y;
+	public int nr_stations;
+	public int nr_docks;
 	public AID id_bike;
 	public int nr_bike_avail;
-	public int nr_stations;
-	public int nr_dock;
 	public int ape;
 	
-	public message_estacao_inform(AID agent, int x, int y, AID id_bike, int nr_bike_avail, int nr_stations, int nr_dock,
+	public message_estacao_inform(AID agent, int x, int y, AID id_bike, int nr_bike_avail, int nr_stations, int nr_docks,
 			int ape) {
 		super();
 		this.agent = agent;
 		this.x = x;
 		this.y = y;
+		this.nr_stations = nr_stations;
+		this.nr_docks = nr_docks;
 		this.id_bike = id_bike;
 		this.nr_bike_avail = nr_bike_avail;
-		this.nr_stations = nr_stations;
-		this.nr_dock = nr_dock;
 		this.ape = ape;
 	}
 
@@ -49,7 +49,31 @@ public class message_estacao_inform implements java.io.Serializable {
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	public int getNr_stations() {
+		return nr_stations;
+	}
 
+	public void setNr_stations(int nr_stations) {
+		this.nr_stations = nr_stations;
+	}
+
+	public int getNr_docks() {
+		return nr_docks;
+	}
+
+	public void setNr_docks(int nr_docks) {
+		this.nr_docks = nr_docks;
+	}
+
+	public int getApe() {
+		return ape;
+	}
+
+	public void setApe(int ape) {
+		this.ape = ape;
+	}
+	
 	public AID getId_bike() {
 		return id_bike;
 	}
@@ -65,35 +89,5 @@ public class message_estacao_inform implements java.io.Serializable {
 	public void setNr_bike_avail(int nr_bike_avail) {
 		this.nr_bike_avail = nr_bike_avail;
 	}
-
-	public int getNr_stations() {
-		return nr_stations;
-	}
-
-	public void setNr_stations(int nr_stations) {
-		this.nr_stations = nr_stations;
-	}
-
-	public int getNr_dock() {
-		return nr_dock;
-	}
-
-	public void setNr_dock(int nr_dock) {
-		this.nr_dock = nr_dock;
-	}
-
-	public int getApe() {
-		return ape;
-	}
-
-	public void setApe(int ape) {
-		this.ape = ape;
-	}
-	
-	
-	
-	
-	
-
 	
 }
